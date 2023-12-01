d4f7d849d13900955ffba4d17f1beb2005884dc4
luizAFS23 <luizafs.1500@gmail.com>
2023-12-01 14:10:29 -0300

    changes

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const myLibrary = [];
 
-function Book(bookname, pagenumber, readitornot){
+function Book(bookname, pagenumber, readitornot, urlimage){
     this.bookname = bookname;
     this.pagenumber = pagenumber;
     this.readitornot = readitornot;
+    this.urlimage = urlimage;
 }
 
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit by J.R.R. Tolkien", "295 pages", "read");
+const theHobbit = new Book("The Hobbit by J.R.R. Tolkien", "295 pages", "read", "https://m.media-amazon.com/images/I/91M9xPIf10L._SL1500_.jpg");
 const lordOfTheRings1 = new Book("The Lord of The Rings: The Fellowship of the King by J.R.R. Tolkien", "576 pages", "not read yet");
-const lordOfTheRings2 = new Book("The Lord of The Rings: The Two Towers by J.R.R. Tolkien", "464 pages", "not read yet");
+const lordOfTheRings2 = new Book("The Lord of The Rings: The Two Towers by J.R.R. Tolkien", "464 pages", "not read yet", "https://m.media-amazon.com/images/I/81lQ5N0QwJL._SL1500_.jpg");
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(lordOfTheRings1);
