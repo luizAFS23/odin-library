fd4ae4024b0a4f243280f66367a123a439be5d63
luizAFS23 <luizafs.1500@gmail.com>
2023-12-01 14:06:04 -0300

    changes

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit by J.R.R. Tolkien", "295 pages", "not read yet");
+const theHobbit = new Book("The Hobbit by J.R.R. Tolkien", "295 pages", "read");
+const lordOfTheRings1 = new Book("The Lord of The Rings: The Fellowship of the King by J.R.R. Tolkien", "576 pages", "not read yet");
+const lordOfTheRings2 = new Book("The Lord of The Rings: The Two Towers by J.R.R. Tolkien", "464 pages", "not read yet");
 
 function displayBooks(){
-    
+    for(let i = 0; i < myLibrary.length; i++){
+
+    }
 }
